25a818bfc8f98fd6bfa3a520d47712dd41230bb3
jtingsanchali <70384906+joe-tingsanchali-sonarsource@users.noreply.github.com>
2025-12-22 14:39:13 -0600

    Fix out of bounds array access in helloworld.cpp
    Correct out of bounds access on scores array.

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -13,4 +13,13 @@ int main()
         cout << word << " ";
     }
     cout << endl;
+
+    int scores[3] = {90, 85, 70};
+
+    // SonarQube will flag this: The array only has indices 0, 1, and 2.
+    // Index 3 is "Out of Bounds".
+    scores[3] = 100; 
+
+    std::cout << scores[3] << std::endl;
+    return 0;
 }
